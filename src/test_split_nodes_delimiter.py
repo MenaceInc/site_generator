a6ed6d9a088390f0bbb7be6009b804b7d79f0b4f
menaceinc <john@menaceinc.com>
2025-07-15 23:08:47 +0100

    Adds split_nodes_delimiter

---
+++ src/test_split_nodes_delimiter.py
@@ -0,0 +1,16 @@
+import unittest
+
+from textnode import TextNode, TextType
+from split_nodes_delimiter import split_nodes_delimiter
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_split_nodes_delimiter(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+
+        self.assertEqual(new_nodes, [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.TEXT),
+        ])
